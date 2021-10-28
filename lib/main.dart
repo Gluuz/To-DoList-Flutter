555c3f04b26c28184ea780e9844f4d4f1cc4d719
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2021-10-28 12:55:29 -0300

    add remove iten

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,19 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
 
+  List _toDoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = jsonDecode(data);
+      });
+    });
+  }
+
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -26,10 +39,9 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+      _saveData();
     });
   }
-
-  List _toDoList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -64,27 +76,40 @@ class _HomeState extends State<Home> {
           ),
           Expanded(
               child: ListView.builder(
-            itemBuilder: (context, index) {
-              return CheckboxListTile(
-                onChanged: (c) {
-                  setState(() {
-                    _toDoList[index]["ok"] = c;
-                  });
-                },
-                title: Text(_toDoList[index]["title"]),
-                value: _toDoList[index]["ok"],
-                secondary: CircleAvatar(
-                  child:
-                      Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
-                ),
-              );
-            },
-            padding: EdgeInsets.only(top: 10.0),
-            itemCount: _toDoList.length,
-          ))
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItems))
         ],
       ),
     );
+  }
+
+  Widget buildItems(context, index) {
+    return Dismissible(
+        key: Key(DateTime.now().microsecondsSinceEpoch.toString()),
+        background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        direction: DismissDirection.startToEnd,
+        child: CheckboxListTile(
+            title: Text(_toDoList[index]["title"]),
+            value: _toDoList[index]["ok"],
+            secondary: CircleAvatar(
+              child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+            ),
+            onChanged: (c) {
+              setState(() {
+                _toDoList[index]["ok"] = c;
+                _saveData();
+              });
+            }));
   }
 
   Future<File> _getFile() async {
